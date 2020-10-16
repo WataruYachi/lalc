db22fea3f1faa45d8456cf7dac712d9efa6c084f
WataruYachi <yatiwataru@gmail.com>
2020-10-16 20:29:24 +0900

    add some parsers

--- src/Parser.hs
+++ src/Parser.hs
@@ -58,3 +58,25 @@ sat p = do
 
 digit :: Parser Char
 digit = sat isDigit
+
+lower :: Parser Char
+lower = sat isLower
+
+upper :: Parser Char
+upper = sat isUpper
+
+letter :: Parser Char
+letter = sat isAlpha
+
+alphanum :: Parser Char
+alphanum = sat isAlphaNum
+
+char :: Char -> Parser Char
+char x = sat (== x)
+
+string :: String -> Parser String
+string [] = return []
+string (x:xs) = do
+    char x
+    string xs
+    return (x:xs)
